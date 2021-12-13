d48895f8513f6d3c89b10228a1166a18f2a18ed0
JooYoung2274 <wndud2274@gmail.com>
2021-12-13 16:51:01 +0900

    router/articles.js update

--- router/articles.js
+++ router/articles.js
@@ -1,23 +1,13 @@
 const express = require("express");
 const articlesController = require("../controller/articles");
 const uploader = require("../middleware/uploader");
-
 const router = express.Router();
 
-router.post(
-  "/",
-  uploader.fields([{ name: "img", maxCount: 4 }]),
-  articlesController.articlePost
-);
+const imageUpload = uploader.fields([{ name: "img", maxCount: 4 }]);
 
+router.post("/", imageUpload, articlesController.articlePost);
 router.get("/", articlesController.articleGet);
-
-router.put(
-  "/:articleId",
-  uploader.fields([{ name: "img", maxCount: 4 }]),
-  articlesController.articleUpdate
-);
-
+router.put("/:articleId", imageUpload, articlesController.articleUpdate);
 router.delete("/:articleId", articlesController.artiecleDelete);
+
 module.exports = router;
-("/api/article");

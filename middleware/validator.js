cdb2bace3da59e21045cdb784ba04497b911b8ed
JooYoung2274 <wndud2274@gmail.com>
2021-12-14 22:46:39 +0900

    validator update

--- middleware/validator.js
+++ middleware/validator.js
@@ -18,7 +18,6 @@ const validatck = (req, res, next) => {
     .json({ message: "비밀번호를 대소문숫자특수문자 포함시켜주세요" });
 };
 
-//영소문, 숫자 혼합 3~12자
 const validateId = [
   body("loginId")
     .isLength({ min: 3 })
@@ -26,7 +25,6 @@ const validateId = [
   validate,
 ];
 
-//길이: 최소 3글자, 10자 제한, 문자 숫자만 가능.
 const validateNick = [
   body("nickname")
     .isLength({ min: 3 })
@@ -34,14 +32,10 @@ const validateNick = [
   validate,
 ];
 
-//길이: 4~12자 제한
-//영대소문숫자특수문자 혼합
 const validatepass = [
   body("password")
     .isLength({ min: 3 })
-    .matches(
-      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{4,12}$/
-    ),
+    .matches(/[a-z]+[A-Z0-9]*$/),
   validatck,
 ];
 
